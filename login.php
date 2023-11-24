2953276788601cc3ab60b006bc7eb6355a8bb6d6
NaufalSyahandra <114036607+NaufalSyahandra@users.noreply.github.com>
2023-11-24 15:34:37 +0700

    Add front end login

--- login.php
+++ login.php
@@ -1,22 +1,16 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+    <title>login</title>
     <meta charset="UTF-8">
-    <title>Sign Up - Sign In</title>
-    <link rel="stylesheet" href="css/loginStyle.css">
-</head>
-<body>
-<!DOCTYPE html>
-<html lang="es" dir="ltr">
-<head>
-    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0">
-    <meta charset="utf-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" type="text/css" href="css/loginStyle.css">
-    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;700;800&display=swap" rel="stylesheet">
 </head>
 <body>
 <main class="main">
-    <div class="container b-container" id="b-container">
+    <div class="container a-container" id="a-container">
         <form class="form" id="b-form" method="post" action="index.html">
             <h2 class="form_title title">Ruang Baca JTI</h2>
             <span class="form__span">Input your username and password</span>
@@ -25,18 +19,17 @@
             <button class="form__button button switch-btn" name="signin">SIGN IN</button>
         </form>
     </div>
-    <div class="switch" id="switch-cnt">
-        <div class="switch__circle"></div>
-        <div class="switch__circle switch__circle--t"></div>
-        <div class="switch__container" id="switch-c1">
-            <h2 class="switch__title title">Welcome Back !</h2>
-            <p class="switch__description description">To keep connected with us please login with your personal
-                info</p>
+    <div class="side">
+        <div class="side__circle"></div>
+        <div class="side__circle side__circle--t"></div>
+        <div class="side__container">
+            <img class="side__logo logo" src="img/Logo%20Polinema%20(Politeknik%20Negeri%20Malang).png" alt="">
+            <h2 class="side__title title">Welcome Back !</h2>
+            <p class="side__description description">*Mahasiswa menggunakan akun siakad
+                <br> *Dosen/Pegawai menggunakan akun Polinema</p>
+
         </div>
     </div>
 </main>
-</body>
-</html>
-<script src="js/loginJs.js"></script>
 </body>
 </html>
