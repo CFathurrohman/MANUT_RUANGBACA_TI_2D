7de25838def93cb66abbaffbafe6a3f0801fcf46
Brilyan Satria Wahyuda <brilyansatria10@gmail.com>
2023-11-29 04:43:12 +0700

    implemented bootstrap to profile view

--- app/views/profile/index.php
+++ app/views/profile/index.php
@@ -2,23 +2,35 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
     <link rel="stylesheet" href="../../../public/css/profileStyle.css">
     <title>User Profile</title>
 </head>
 <body>
 
-    <div class="content-container">
-        <div class="text-wrapper">Brilyan Satria Wahyuda</div>
-        <div class="text-wrapper-2">D-IV Teknik Informatika/2241720019</div>
+    <div class="container">
+        <div class="row">
+            <div class="col">
+                <div class="content-container">
+                    <div class="text-wrapper">Brilyan Satria Wahyuda</div>
+                    <div class="text-wrapper-2">D-IV Teknik Informatika/2241720019</div>
+                </div>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col">
+                <div class="button-container">
+                    <button class="btn btn-primary" id="changePassword" style="margin-right: 20px;">Ganti Password</button>
+                    <button class="btn btn-danger" id="logout">Keluar</button>
+                </div>
+            </div>
+        </div>
     </div>
 
-    <div class="button-container">
-        <div class="button" id="changePassword">Ganti Password</div>
-        <div class="button2" id="logout">Keluar</div>
-    </div>
-
+   
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
