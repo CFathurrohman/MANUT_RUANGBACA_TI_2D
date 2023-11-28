1b422a10190e3084bea348297ebef6f1ac1643b1
Cyndu Fathur Rohman <cfathurrohman@gmail.com>
2023-11-29 00:08:13 +0700

    Add more funtion

--- app/core/Database.php
+++ app/core/Database.php
@@ -56,4 +56,13 @@
             $this->execute();
             return $this->stmt->fetchAll(PDO::FETCH_ASSOC);
         }
+
+        public function single(){
+            $this->execute();
+            return $this->stmt->fetch(PDO::FETCH_ASSOC);
+        }
+
+        public function rowCount(){
+            return $this->stmt->rowCount();
+        }
     }
