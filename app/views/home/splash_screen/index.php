6aa3ed74c0de9c77d042a630a081feca24e3f9a3
Brilyan Satria Wahyuda <brilyansatria10@gmail.com>
2023-11-29 16:53:00 +0700

    finsihed splashScreen

--- app/views/home/splash_screen/index.php
+++ app/views/home/splash_screen/index.php
@@ -1,22 +1,26 @@
 <!doctype html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>SplashScreen</title>
-    <link rel="stylesheet" type="text/css" href="css/splashStyle.css">
+    <link rel="stylesheet" type="text/css" href="../../../../public/css/splashStyle.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
 </head>
 <body>
-<div class="ruang_baca">
-    <div>
+    <div class="card-container">
+        <div class="text-center">
+            <img class="logo" src="../../../../public/img/Logo Ruang Baca.png" alt="Logo">
+        </div>
+        <div class="text-center mt-5">
+            <h1 class="text-white font-weight-bold">Ruang Baca JTI</h1>
+        </div>
     </div>
-    <div>
-        <p>Ruang Baca JTI</p>
-    </div>
-    <div>
-    </div>
-</div>
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
 </body>
+
 </html>
