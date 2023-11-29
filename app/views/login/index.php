f6ebd827df4ba7af87e608ddc021a0e96298b65f
Brilyan Satria Wahyuda <brilyansatria10@gmail.com>
2023-11-29 19:48:26 +0700

    finsihed login view

--- app/views/login/index.php
+++ app/views/login/index.php
@@ -6,32 +6,27 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-core    <link rel="stylesheet" type="text/css" href="../../../public/css/loginStyle.css">
+    <link rel="stylesheet" type="text/css" href= "../../../public/css/loginStyle.css"    >
 </head>
 <body>
-<main>
+<main class="main">
     <div class="container a-container" id="a-container">
-        // Path belum bener
-        <form method="post" action="../../models/cek_login.php">
+        <form class="form" id="b-form" method="post" action="index.html">
             <h2 class="form_title title">Ruang Baca JTI</h2>
-            <span class="form__span">Input your username and password</span>
-            <div>
-                <input class="form__input" type="text" name="username" placeholder="Username" required>
-            </div>
-            <div>
-                <input class="form__input" type="password" name="password" placeholder="Password" required>
-            </div>
-            <button class="form__button button switch-btn" name="signin" type="submit">SIGN IN</button>
+            <span class="form__span">Masukkan nama pengguna dan kata sandi</span>
+            <input class="form__input" type="text" placeholder="Username" name="Username">
+            <input class="form__input" type="password" placeholder="Password" name="password">
+            <button class="form__button button switch-btn" name="signin">SIGN IN</button>
         </form>
     </div>
     <div class="side">
         <div class="side__circle"></div>
         <div class="side__circle side__circle--t"></div>
         <div class="side__container">
-            <img class="side__logo logo" src="../../../public/img/bulat%20putih.png" alt="">
+            <img class="side__logo logo" src="../../../public/img/Logo Ruang Baca.png" alt="">
             <h2 class="side__title title">Welcome Back !</h2>
-            <p class="side__description description">*Mahasiswa menggunakan akun siakad
-                <br> *Dosen/Pegawai menggunakan akun Polinema</p>
+            <p class="side__description description">Mahasiswa menggunakan akun siakad
+                <br> Dosen/Pegawai menggunakan akun Polinema</p>
 
         </div>
     </div>
