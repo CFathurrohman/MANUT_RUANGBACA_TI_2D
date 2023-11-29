37102d392c0ddc007a8da65e80b1cc2160e11fce
Brilyan Satria Wahyuda <brilyansatria10@gmail.com>
2023-11-30 01:05:16 +0700

    finshed footer

---
+++ app/views/templates/footer.php
@@ -0,0 +1,30 @@
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Footer</title>
+    <link rel="stylesheet" href="../../../public/css/footerStyle.css">
+</head>
+
+<body>
+    <footer>
+        <ul class="icon">
+            <li><a href="#"><ion-icon name="library-outline" /a></li>
+        </ul>
+        <ul class="menu">
+            <li><a href="#">HOME</a></li>
+        </ul>
+        <div class="waves">
+            <div class="wave" id="wave1"></div>
+            <div class="wave" id="wave2"></div>
+            <div class="wave" id="wave3"></div>
+            <div class="wave" id="wave4"></div>
+
+        </div>
+
+        <p>Kelompok 2 | All Rights Reserved</p>
+    </footer>
+    <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
+    <script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
+
+</html>
