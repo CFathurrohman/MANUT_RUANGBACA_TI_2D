e6beaa100bc782bbcfc3ddd6c95721b532a9a439
Brilyan Satria Wahyuda <brilyansatria10@gmail.com>
2023-12-01 02:32:46 +0700

    changed header color

--- app/views/templates/header.php
+++ app/views/templates/header.php
@@ -1,10 +1,11 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Halaman <?= $data['judul']; ?></title>
-    <link rel="stylesheet" type="text/css" href="<?= BASEURL;?>/css/headerStyle.css">
+    <link rel="stylesheet" type="text/css" href="<?= BASEURL; ?>/css/headerStyle.css">
 
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
     <link rel="stylesheet" href="<?= BASEURL; ?>/css/bootstrap.css">
@@ -13,7 +14,7 @@
 <header>
     <nav>
         <div class="container">
-            <a href="http://localhost/manut_ruangbaca_ti_2d/public/Home"><img class="logo" src="<?= BASEURL;?>/img/Logo Bibliophile.png" alt="logo" width="198" height="48"></a>
+            <a href="http://localhost/manut_ruangbaca_ti_2d/public/Home"><img class="logo" src="<?= BASEURL; ?>/img/Logo Bibliophile.png" alt="logo" width="198" height="48"></a>
             <ul class="nav__links">
                 <li class="active"><a href="http://localhost/manut_ruangbaca_ti_2d/public/Home"><i class="fa fa-home"></i> Dashboard</a></li>
                 <li><a href="#"><i class="fa fa-book"></i> Buku</a></li>
@@ -28,9 +29,11 @@
                 <li class="search-box">
                     <input type="text" placeholder="Search">
                 </li>
+
                 <li id="dark-mode-toggle"><a href="#"><i class="fa fa-sun"></i></a></li>
                 <li><a href="#"><i class="fa fa-shopping-cart"></i></a></li>
                 <li><a href="#"><i class="fa fa-user"></i></a></li>
+
             </ul>
         </div>
     </nav>
@@ -45,4 +48,5 @@
 
     });
 </script>
+
 <body>
