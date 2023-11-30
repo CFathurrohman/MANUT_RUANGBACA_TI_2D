0a7412b9826638e57934748fba005db9d583e57f
Cyndu Fathur Rohman <cfathurrohman@gmail.com>
2023-11-30 22:08:02 +0700

    Adding scripts

--- app/views/templates/footer.php
+++ app/views/templates/footer.php
@@ -1,9 +1,19 @@
-<link rel="stylesheet" type="text/css" href="<?= BASEURL;?>/css/footerStyle.css">
+<link rel="stylesheet" type="text/css" href="<?= BASEURL; ?>/css/footerStyle.css">
+<script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
+<script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
+
+// Bootstrap
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+
+// Jquery & js
+<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+<script src="<?= BASEURL; ?>/js/script.js"></script>
+
 <footer>
     <ul class="icon">
         <li><a href="#">
-                <ion-icon name="library-outline"
-                /a></li>
+                <ion-icon name="library-outline" /a></li>
     </ul>
     <ul class="menu">
         <li><a href="#">HOME</a></li>
@@ -19,7 +29,6 @@
     <p>Kelompok 2 | All Rights Reserved</p>
 </footer>
 
-<script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
-<script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
 </body>
+
 </html>
--- app/views/templates/header.php
+++ app/views/templates/header.php
@@ -7,7 +7,6 @@
     <link rel="stylesheet" type="text/css" href="<?= BASEURL;?>/css/headerStyle.css">
 
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
 </head>
 
 <header>
@@ -42,6 +41,7 @@
 
     darkModeToggle.addEventListener('click', () => {
         body.classList.toggle('dark-mode');
+
     });
 </script>
 <body>
