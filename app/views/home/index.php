78aa4f97ab69f016d18226c3bd67af655ba355b5
NaufalSyahandra <114036607+NaufalSyahandra@users.noreply.github.com>
2023-12-01 14:57:43 +0700

    update home/index and buku

--- app/views/home/index.php
+++ app/views/home/index.php
@@ -3,7 +3,7 @@
         <?php foreach ($data['buku'] as $buku) : ?>
             <div class="col">
                 <div class="card">
-                    <img src="<?= BASEURL; ?>/img/c++.jpg" class="card-img-top" alt="" style="width: 200px; height: 300px">
+                    <img src="<?= BASEURL; ?>/img/c++.jpg" class="card-img-top" alt="" style="width: 195px; height: 300px">
                     <div class="card-body">
                         <h5 class="card-title"><?= $buku['nama_buku']; ?></h5>
                         <ul class="list-group list-group-flush">
@@ -14,6 +14,9 @@
                             <li class="list-group-item"><small class="text-muted"><?= $buku['tahun_terbit']; ?></small>
                             </li>
                         </ul>
+                        <div class="card-body text-end">
+                            <a href="<?= BASEURL; ?>/buku/read/<?= $buku['id']; ?>" class="btn btn-warning"">Edit</a>
+                        </div>
                     </div>
                 </div>
             </div>
