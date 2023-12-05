70c167ad0db0f14011a4346b7cfb3316b76fa3eb
Cyndu Fathur Rohman <cfathurrohman@gmail.com>
2023-12-05 14:39:45 +0700

    remove and add ajax script

--- app/views/templates/footer.php
+++ app/views/templates/footer.php
@@ -8,7 +8,6 @@
 
 <!-- // Jquery & js -->
 <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-<script src="<?= BASEURL; ?>/js/script.js"></script>
 
 <footer>    
 <section>
--- app/views/templates/header.php
+++ app/views/templates/header.php
@@ -8,7 +8,7 @@
     <link rel="stylesheet" href="style.css">
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
     <link rel="stylesheet" type="text/css" href="<?= BASEURL; ?>/css/headerStyle.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 </head>
 
 <body>
