d43d6978579541019c89045f329457dfc5003f3f
Cyndu Fathur Rohman <cfathurrohman@gmail.com>
2023-12-13 04:10:04 +0700

    Fix title

--- app/views/pengembalian/index.php
+++ app/views/pengembalian/index.php
@@ -9,7 +9,7 @@
 <div class="container mt-5">
     <div class="row">
         <div class="col-12"><br>
-            <h3>Riwayat</h3><br>
+            <h3>Pengembalian Buku</h3><br>
             <hr style="height: 1px;color: black;background-color: black;">
         </div>
     </div>
