3bbc7ead0358e7c05ff8809a56406f3cd7deaf69
Brilyan Satria Wahyuda <brilyansatria10@gmail.com>
2023-12-13 20:49:52 +0700

    modified header

--- app/views/templates/header.php
+++ app/views/templates/header.php
@@ -47,6 +47,12 @@
                 echo '                    </li>';
                 echo '                    <li>';
                 echo '                        <div class="item-title">';
+                echo '                                <a href="http://localhost/manut_ruangbaca_ti_2d/public/Pengembalian"><h3>Pengembalian</h3></a>';
+                echo '                                <a href="http://localhost/manut_ruangbaca_ti_2d/public/Pengembalian"><p>Akses Pengenmbalian</p></a>';
+                echo '                        </div>';
+                echo '                    </li>';
+                echo '                    <li>';
+                echo '                        <div class="item-title">';
                 echo '                            <a href="http://localhost/manut_ruangbaca_ti_2d/public/Riwayat"><h3>Riwayat</h3></a>';
                 echo '                            <a href="http://localhost/manut_ruangbaca_ti_2d/public/Riwayat"><p>Akses Riwayat</p>';
                 echo '                        </div>';
@@ -72,32 +78,21 @@
                 echo '<div class="action-buttons hide">';
                 echo '<nav>';
                 echo '<ul class="navigation hide">';
+
+
                 echo '    <li>';
-                echo '        <button>
-                             <svg class="icon" xmlns="http://www.w3.org/2000/svg" height="18" width="30" viewBox="0 0 576 512">
+                echo '             
+                <a href="http://localhost/manut_ruangbaca_ti_2d/public/Keranjang">                <svg class="icon" xmlns="http://www.w3.org/2000/svg" height="18" width="30" viewBox="0 0 576 512">
                                 <path class="icon-path" fill="#fafafa"
                                     d="M0 24C0 10.7 10.7 0 24 0H69.5c22 0 41.5 12.8 50.6 32h411c26.3 0 45.5 25 38.6 50.4l-41 152.3c-8.5 31.4-37 53.3-69.5 53.3H170.7l5.4 28.5c2.2 11.3 12.1 19.5 23.6 19.5H488c13.3 0 24 10.7 24 24s-10.7 24-24 24H199.7c-34.6 0-64.3-24.6-70.7-58.5L77.4 54.5c-.7-3.8-4-6.5-7.9-6.5H24C10.7 48 0 37.3 0 24zM128 464a48 48 0 1 1 96 0 48 48 0 1 1 -96 0zm336-48a48 48 0 1 1 0 96 48 48 0 1 1 0-96z"/>
                             </svg>';
-                echo '        </button>';
-                echo '        <div class="dropdown__wrapper__keranjang">';
-                echo '            <div class="dropdown">';
-                echo '                <ul class="list-items-with-description">';
-                echo '                    <li>';
-                echo '                        <div class="item-title">';
-                echo '                                <a href="http://localhost/manut_ruangbaca_ti_2d/public/Keranjang"><h3>Nama Buku</h3></a>';
-                //            echo '                                <a href="http://localhost/manut_ruangbaca_ti_2d/public/Peminjaman"><p>...</p></a>';
-                echo '                        </div>';
-                echo '                    </li>';
-
-                echo '                </ul>';
-                echo '            </div>';
-                echo '        </div>';
-                echo '    </li>';
                 echo '    <li>';
                 echo '        <button>
                           <a class="" style="color: #fafafa"><svg xmlns="http://www.w3.org/2000/svg" height="16" width="14" viewBox="0 0 448 512">
                                 <path fill="#fafafa" 
                                     d="M224 256A128 128 0 1 0 224 0a128 128 0 1 0 0 256zm-45.7 48C79.8 304 0 383.8 0 482.3C0 498.7 13.3 512 29.7 512H418.3c16.4 0 29.7-13.3 29.7-29.7C448 383.8 368.2 304 269.7 304H178.3z"/></svg>';
+
+
                 echo '       ' . $_SESSION['username'] . ' </a></button>';
                 echo '        <div class="dropdown__wrapper__akun">';
                 echo '            <div class="dropdown">';
@@ -130,15 +125,6 @@
                 echo '</div>';
             }
         } else {
-            echo '    <li>';
-            echo '        <a href="http://localhost/manut_ruangbaca_ti_2d/public/Home" title="Menuju Ke Katalog"><i class="fa fa-home"></i>&nbsp;Katalog</a>';
-            echo '    </li>';
-            echo '</ul>';
-            echo '</nav>';
-            echo '</div>';
-            echo '<div class="action-buttons hide">';
-            echo '<a href="http://localhost/manut_ruangbaca_ti_2d/public/Log" title="Log in" class="login">Masuk</a>';
-            echo '</div>';
         }
         ?>
     </header>
