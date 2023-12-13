43f83eff205e3f15b9fb68d52ee47f35a689b4cd
Brilyan Satria Wahyuda <brilyansatria10@gmail.com>
2023-12-14 03:33:39 +0700

    created carousel

--- app/views/home/index.php
+++ app/views/home/index.php
@@ -1,3 +1,7 @@
+<link rel="stylesheet" type="text/css" href="<?= BASEURL; ?>/css/homeStyle.css">
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
+
 <div class="transition-group">
     <div></div>
     <div></div>
@@ -10,10 +14,41 @@
     <div class="row">
         <div class="col-12"><br>
             <h3>Katalog buku</h3><br>
-            <hr style="height: 1px;color: black;background-color: black;">
+           
         </div>
     </div>
+    <div class="carousel">
+    <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+        <!-- <ol class="carousel-indicators">
+            <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+            <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+            <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+        </ol> -->
+        <div class="carousel-inner">
+            <div class="carousel-item active fade-in">
+                <img class="d-block w-100" src="https://source.unsplash.com/78A265wPiO4" alt="First slide">
+            </div>
+            <div class="carousel-item fade-in">
+                <img class="d-block w-100" src="https://source.unsplash.com/eOpewngf68w" alt="Second slide">
+            </div>
+            <div class="carousel-item fade-in">
+                <img class="d-block w-100" src="https://source.unsplash.com/ndN00KmbJ1c" alt="Third slide">
+            </div>
+        </div>
+        <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            
+        </a>
+        <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+           
+        </a>
+    </div>
+</div>
 
+
+
+ <hr style="height: 1px;color: black;background-color: black;">
     <div class="row mb-3">
         <div class="col-lg-12 d-flex justify-content-end">
             <form action="<?= BASEURL; ?>/home/cari" method="post" class="d-flex">
