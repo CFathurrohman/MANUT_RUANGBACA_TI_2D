cde9e1becf009c4b64980606fefe340b081c6096
Cyndu Fathur Rohman <cfathurrohman@gmail.com>
2023-12-16 00:11:43 +0700

    Remove history from profile

--- app/views/profil/index.php
+++ app/views/profil/index.php
@@ -5,6 +5,7 @@
     <div></div>
     <div></div>
 </div>
+
 <body>
     <link rel="stylesheet" type="text/css" href="<?= BASEURL; ?>/css/profileStyle.css">
 
@@ -20,53 +21,5 @@
         </div>
     </div>
 
-    
+
     <div class="content">
-    <div class="content-title">
-    <h3>Buku yang sedang dipinjam</h3> 
-</div>
-       
-<div class="row row-cols-1 row-cols-md-6 g-4">
-        <?php foreach ($data['buku_pinjam'] as $buku) : ?>
-
-            <div class="col">
-                <div class="card">
-                    <img src="<?= BASEURL; ?>/img/<?= $buku['gambar_buku']; ?>" class="card-img-top" alt="" style="width: 195px; height: 300px">
-
-                    <div class="card-body">
-                        <h5 class="card-title"><?= $buku['nama_buku']; ?></h5>
-                        <li class="list-group-item"><small class="text-muted"><?= $buku['penulis']; ?></small></li><br>
-                        <li class="list-group-item"><small class="text-muted"><?= $buku['tahun_terbit']; ?></small></li>
-                        <div class="card-body text-end">
-                            <a href="<?= BASEURL; ?>/profil/read/<?= $buku['id_buku']; ?>" class="btn btn-outline-info">Lihat</a>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        <?php endforeach; ?>
-    </div>
-</div>
-<div class="content">
-    <div class="content-title">
-        <h3>Riwayat Peminjaman Selesai</h3> 
-</div>
-<div class="row row-cols-1 row-cols-md-6 g-4">
-        <?php foreach ($data['buku_kembali'] as $buku) : ?>
-
-            <div class="col">
-                <div class="card">
-                    <img src="<?= BASEURL; ?>/img/<?= $buku['gambar_buku']; ?>" class="card-img-top" alt="" style="width: 195px; height: 300px">
-
-                    <div class="card-body">
-                        <h5 class="card-title"><?= $buku['nama_buku']; ?></h5>
-                        <li class="list-group-item"><small class="text-muted"><?= $buku['penulis']; ?></small></li><br>
-                        <li class="list-group-item"><small class="text-muted"><?= $buku['tahun_terbit']; ?></small></li>
-                        <div class="card-body text-end">
-                            <a href="<?= BASEURL; ?>/profil/read/<?= $buku['id_buku']; ?>" class="btn btn-outline-info">Lihat</a>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        <?php endforeach; ?>
-    </div>
-</div>
