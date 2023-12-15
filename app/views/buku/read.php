2a8978e78acec3838b2008a5fbed3cec0a354c6e
Cyndu Fathur Rohman <cfathurrohman@gmail.com>
2023-12-15 11:21:28 +0700

    Add profile menu

--- app/views/buku/read.php
+++ app/views/buku/read.php
@@ -53,49 +53,6 @@
                 </div>
             </div>
         </div>
-        <!-- <div class="row row-cols-1 row-cols-md-5 g-4" ">
-<div class=" col">
-            <div class="card">
-                <img src="..." class="card-img-top" alt="...">
-                <div class="card-body">
-                    <h5 class="card-title">Card title</h5>
-                    <p class="card-text">This is a longer card with supporting text below as a natural lead-in to
-                        additional content. This content is a little bit longer.</p>
-                </div>
-            </div>
-        </div>
-        <div class="col">
-            <div class="card">
-                <img src="..." class="card-img-top" alt="...">
-                <div class="card-body">
-                    <h5 class="card-title">Card title</h5>
-                    <p class="card-text">This is a longer card with supporting text below as a natural lead-in to
-                        additional content. This content is a little bit longer.</p>
-                </div>
-            </div>
-        </div>
-
-        <div class="col">
-            <div class="card">
-                <img src="..." class="card-img-top" alt="...">
-                <div class="card-body">
-                    <h5 class="card-title">Card title</h5>
-                    <p class="card-text">This is a longer card with supporting text below as a natural lead-in to
-                        additional content.</p>
-                </div>
-            </div>
-        </div>
-        <div class="col">
-            <div class="card">
-                <img src="..." class="card-img-top" alt="...">
-                <div class="card-body">
-                    <h5 class="card-title">Card title</h5>
-                    <p class="card-text">This is a longer card with supporting text below as a natural lead-in to
-                        additional content. This content is a little bit longer.</p>
-                </div>
-            </div>
-        </div>
-    </div> -->
     </div>
 </div>
 
--- app/views/profil/index.php
+++ app/views/profil/index.php
@@ -1,5 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
 <div class="transition-group">
     <div></div>
     <div></div>
@@ -7,21 +5,17 @@
     <div></div>
     <div></div>
 </div>
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
-    <link rel="stylesheet" type="text/css" href="<?= BASEURL; ?>/css/profileStyle.css">
-</head>
-
 <body>
+    <link rel="stylesheet" type="text/css" href="<?= BASEURL; ?>/css/profileStyle.css">
 
     <div class="profile-container">
         <div class="profile">
             <img class="profile-image" src="">
             <div class="profile-details">
-                <div class="profile-name">Ziraaa</div>
-                <div class="profile-info">Information</div>
+                <div class="profile-name"><?= $data['user']['nama']; ?></div>
+                <div class="profile-info">Status : <?= $data['user']['status']; ?> </div>
+                <div class="profile-info"> ID : <?= $data['user']['id_anggota']; ?> </div>
+                <div class="profile-info">No Telepon : <?= $data['user']['no_telp']; ?> </div>
             </div>
         </div>
     </div>
@@ -32,24 +26,47 @@
     <h3>Buku yang sedang dipinjam</h3> 
 </div>
        
-    <div class="book">
-        <div class="row row-cols-1 row-cols-md-5 g-4 ">
-        <div class=" col">
-            <div class="card">
-                <img src="..." class="card-img-top" alt="...">
-                <div class="card-body">
-                    <h5 class="card-title">Card title</h5>
-                    <p class="card-text">This is a longer card with supporting text below as a natural lead-in to
-                        additional content. This content is a little bit longer.</p>
+<div class="row row-cols-1 row-cols-md-6 g-4">
+        <?php foreach ($data['buku_pinjam'] as $buku) : ?>
+
+            <div class="col">
+                <div class="card">
+                    <img src="<?= BASEURL; ?>/img/<?= $buku['gambar_buku']; ?>" class="card-img-top" alt="" style="width: 195px; height: 300px">
+
+                    <div class="card-body">
+                        <h5 class="card-title"><?= $buku['nama_buku']; ?></h5>
+                        <li class="list-group-item"><small class="text-muted"><?= $buku['penulis']; ?></small></li><br>
+                        <li class="list-group-item"><small class="text-muted"><?= $buku['tahun_terbit']; ?></small></li>
+                        <div class="card-body text-end">
+                            <a href="<?= BASEURL; ?>/profil/read/<?= $buku['id_buku']; ?>" class="btn btn-outline-info">Lihat</a>
+                        </div>
+                    </div>
                 </div>
             </div>
-        </div>
+        <?php endforeach; ?>
     </div>
+</div>
+<div class="content">
+    <div class="content-title">
+        <h3>Riwayat Peminjaman Selesai</h3> 
+</div>
+<div class="row row-cols-1 row-cols-md-6 g-4">
+        <?php foreach ($data['buku_kembali'] as $buku) : ?>
+
+            <div class="col">
+                <div class="card">
+                    <img src="<?= BASEURL; ?>/img/<?= $buku['gambar_buku']; ?>" class="card-img-top" alt="" style="width: 195px; height: 300px">
+
+                    <div class="card-body">
+                        <h5 class="card-title"><?= $buku['nama_buku']; ?></h5>
+                        <li class="list-group-item"><small class="text-muted"><?= $buku['penulis']; ?></small></li><br>
+                        <li class="list-group-item"><small class="text-muted"><?= $buku['tahun_terbit']; ?></small></li>
+                        <div class="card-body text-end">
+                            <a href="<?= BASEURL; ?>/profil/read/<?= $buku['id_buku']; ?>" class="btn btn-outline-info">Lihat</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        <?php endforeach; ?>
     </div>
-  
-
-
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
-</body>
-
-</html>
+</div>
