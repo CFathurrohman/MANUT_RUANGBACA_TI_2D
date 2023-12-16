4efbbe0e9a24f6e3d45f8457f6312a171976f6ea
Cyndu Fathur Rohman <cfathurrohman@gmail.com>
2023-12-16 18:22:33 +0700

    FIx buku Query

--- app/models/Buku_model.php
+++ app/models/Buku_model.php
@@ -12,7 +12,7 @@
 
     public function getAllBuku()
     {
-        $this->db->query('SELECT a.*, k.nama_kategori FROM ' . $this->table . ' a INNER JOIN kategori k ON a.id_kategori=k.id_ktgr');
+        $this->db->query('SELECT a.*, k.nama_kategori FROM ' . $this->table . ' a INNER JOIN kategori k ON a.id_kategori=k.id_ktgr WHERE is_deleted IS NULL');
         return $this->db->resultSet();
     }
 
@@ -30,10 +30,10 @@
 
     public function getBukuById($id)
     {
-        $this->db->query('SELECT * FROM ' . $this->table . ' WHERE id_buku=:id_buku');
+        $this->db->query('SELECT * FROM ' . $this->table . ' WHERE id_buku=:id_buku AND is_deleted IS NULL');
         $this->db->bind(':id_buku', $id);
         return $this->db->single();
-    }    
+    }
 
     public function tambahDataBuku($data, $gambar)
     {
@@ -48,10 +48,10 @@
         $this->db->bind(':deskripsi', $data['deskripsi']);
         $this->db->bind(':id_kategori', $data['id_kategori']);
         $this->db->bind(':gambar_buku', $gambar);
-    
+
         $this->db->execute();
         return $this->db->rowCount();
-    } 
+    }
 
     public function cariDataBuku()
     {
@@ -60,7 +60,7 @@
         $query = "SELECT b.*, k.nama_kategori 
               FROM buku b 
               INNER JOIN kategori k ON b.id_kategori = k.id_ktgr 
-              WHERE b.nama_buku LIKE :keyword";
+              WHERE b.nama_buku LIKE :keyword AND is_deleted IS NULL";
 
         $this->db->query($query);
         $this->db->bind(':keyword', "%$keyword%");
@@ -70,14 +70,10 @@
 
     public function hapusDataBuku($id)
     {
-        $deleteQuery = " DELETE FROM buku
-                        WHERE id_buku = :id_buku";
-
+        $deleteQuery = "UPDATE buku SET is_deleted = CURDATE() WHERE id_buku = :id_buku";
         $this->db->query($deleteQuery);
         $this->db->bind(":id_buku", $id);
         $this->db->execute();
-
-        return $this->db->resultSet();  
     }
 
     public function ubahDataBuku($data, $gambar)
