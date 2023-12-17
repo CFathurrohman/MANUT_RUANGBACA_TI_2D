c2dc298466117c6bad29af2cbab0043f636f4520
Cyndu Fathur Rohman <cfathurrohman@gmail.com>
2023-12-17 14:58:57 +0700

    Modify paging

--- app/views/anggota/index.php
+++ app/views/anggota/index.php
@@ -70,17 +70,79 @@
             </tbody>
         </table>
     </div>
+
     <nav aria-label="Page navigation example">
         <form action="<?= BASEURL; ?>/anggota" method="post">
             <ul class="pagination justify-content-center">
-                <?php for ($i = 1; $i <= $data['total_pages']; $i++) : ?>
-                    <li class="page-item <?= ($i == $data['page']) ? 'active' : '' ?>">
+                <?php
+                $totalPages = $data['total_pages'];
+                $currentPage = $data['page'];
+
+                // Previous Button
+                ?>
+                <li class="page-item <?= ($currentPage == 1) ? 'disabled' : '' ?>">
+                    <button type="submit" name="page" value="<?= max(1, $currentPage - 1) ?>" class="page-link">&laquo; </button>
+                </li>
+                <?php
+
+                // First Button
+                ?>
+                <li class="page-item <?= ($currentPage == 1) ? 'active' : '' ?>">
+                    <button type="submit" name="page" value="1" class="page-link">1</button>
+                </li>
+                <?php
+
+                // Ellipsis Before First
+                if ($currentPage > 3) {
+                ?>
+                    <li class="page-item disabled">
+                        <button type="button" class="page-link">...</button>
+                    </li>
+                <?php
+                }
+
+                // Numbered Buttons
+                $startPage = max(2, $currentPage - 1);
+                $endPage = min($startPage + 2, $totalPages);
+
+                for ($i = $startPage; $i <= $endPage; $i++) {
+                ?>
+                    <li class="page-item <?= ($i == $currentPage) ? 'active' : '' ?>">
                         <button type="submit" name="page" value="<?= $i ?>" class="page-link"><?= $i ?></button>
                     </li>
-                <?php endfor; ?>
+                <?php
+                }
+
+                // Ellipsis Before Last
+                if ($totalPages - $currentPage > 2 && $totalPages > 5) {
+                ?>
+                    <li class="page-item disabled">
+                        <button type="button" class="page-link">...</button>
+                    </li>
+                <?php
+                }
+
+                // Last Button
+                if ($currentPage < $totalPages && $currentPage != $totalPages - 1 &&  $totalPages > 4) {
+                ?>
+                    <li class="page-item">
+                        <button type="submit" name="page" value="<?= $totalPages ?>" class="page-link"><?= $totalPages ?></button>
+                    </li>
+                <?php
+                }
+
+                // Next Button
+                ?>
+                <li class="page-item <?= ($currentPage == $totalPages) ? 'disabled' : '' ?>">
+                    <button type="submit" name="page" value="<?= min($totalPages, $currentPage + 1) ?>" class="page-link"> &raquo;</button>
+                </li>
+                <?php
+                ?>
             </ul>
         </form>
     </nav>
+
+
 </div>
 
 <!-- Modal  -->
